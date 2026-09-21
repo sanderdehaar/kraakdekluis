0222094da8ceb7549c5739c5a1b80b89055371e7
sanderdehaar <sanderdehaart@live.nl>
2026-09-21 19:50:29 +0200

    Updated redirection homepage.

--- src/components/layout/BrandHeader/BrandHeader.tsx
+++ src/components/layout/BrandHeader/BrandHeader.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom'
 import './CollaborationLogo.css'
 
 import fantaLogo from '/images/fanta_logo.svg'
@@ -10,23 +11,23 @@ type BrandHeaderProps = {
 function BrandHeader({ variant = 'default' }: BrandHeaderProps) {
   return (
     <header className={`collaboration-header ${variant === 'locations' ? 'collaboration-header--locations' : ''}`}>
-      <a href="https://www.fanta.nl/" target="_blank" rel="noreferrer" aria-label="Visit Fanta">
+      <Link to="/" aria-label="Visit Fanta">
         <img
           src={fantaLogo}
           alt="Fanta"
           className="collaboration-header__image collaboration-header__image--fanta"
         />
-      </a>
+      </Link>
 
       <span className="collaboration-header__separator">×</span>
 
-      <a href="https://www.stuktv.nl/" target="_blank" rel="noreferrer" aria-label="Visit StukTV">
+      <Link to="/" aria-label="Visit StukTV">
         <img
           src={stukLogo}
           alt="StukTV"
           className="collaboration-header__image collaboration-header__image--stuk"
         />
-      </a>
+      </Link>
     </header>
   )
 }
